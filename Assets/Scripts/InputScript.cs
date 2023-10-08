d09cd5ef70495a5902b7df1671b3f802a4ae4eb1
unknown <kzhang.moobit@gmail.com>
2023-10-08 16:09:31 -0400

    settings intitial function

---
+++ Assets/Scripts/InputScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InputScript : MonoBehaviour
+{
+    private TMP_InputField inputField;
+    [SerializeField] string type = "name";
+    private SettingsController settingsController;
+    string text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputField = GetComponent<TMP_InputField>();
+        settingsController = GameObject.FindGameObjectsWithTag("SettingsController")[0].GetComponent<SettingsController>();
+        
+        switch (type)
+        {
+            case "name":
+                text = settingsController.GetPlayerName();
+                break;
+            case "room":
+                text = settingsController.GetRoomName();
+                break;
+        }
+
+        inputField.text = text != "" ? text : "";
+
+    } 
+}
